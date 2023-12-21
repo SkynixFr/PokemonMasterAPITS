7aebd89b552ae6ceb558fd1618db797b4fcf2c9d
Ricky Khao <khaoricky@gmail.com>
2023-12-21 16:07:28 +0100

    feat(vitest) : remplacement de jest par vitest

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '../../libs/prisma';
 
 //  Récupération de tous les utilisateurs
 export const getUsers = async () => {
